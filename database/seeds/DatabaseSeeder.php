d72e216b21e06d3629d75a52145c8677ed3d5982
llipiterdev <be.calderong@outlook.com>
2019-06-06 23:19:11 -0500

    Trabajo con Seeders y Model Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncate_tables([
+            'rol',
+            'permiso'
+        ]);
         // $this->call(UsersTableSeeder::class);
+        $this->call(rolTableSeeder::class);
+        $this->call(PermisoTableSeeder::class);
+
+    }
+
+    protected function truncate_tables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
