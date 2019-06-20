093423a8da8102c2decb0aacb8a6cdc484277bbf
llipiterdev <be.calderong@outlook.com>
2019-06-19 23:35:57 -0500

    CRUD menu

---
+++ resources/views/admin/permiso/crear.blade.php
@@ -0,0 +1,19 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+    Sistema Permiso
+@endsection
+
+@section('contenido')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Crear Permiso</h3>
+                </div>
+                <div class="box-body">
+                    Aqui el form
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
